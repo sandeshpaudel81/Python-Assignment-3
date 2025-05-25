9032391e37fb9a34d097d845a2097b9d211dc982
binod basnet <vinodbasnet87@gmail.com>
2025-05-25 20:06:39 +1000

    feat : Draw human like enemy

--- question2.py
+++ question2.py
@@ -258,6 +258,115 @@ class Enemy(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft=(x, y))
         self.max_health = self.health
         self.direction = 1 # 1 for right, -1 for left (for patrolling)
+        
+    def draw_character(self, surface, body_color):
+        """Draws a human-like enemy onto the given surface with more detail."""
+        surface.fill((0, 0, 0, 0))
+
+       # Draw head circular shape
+        head_radius = self.width // 3
+        head_center_x = self.width // 2
+        head_center_y = head_radius + 2
+        pygame.draw.circle(surface, GRAY, (head_center_x, head_center_y), head_radius) # Enemy head color
+
+        # Neck 
+        neck_width = self.width * 0.2
+        neck_height = self.height * 0.05
+        neck_x = (self.width - neck_width) // 2
+        neck_y = head_center_y + head_radius - 2
+        pygame.draw.rect(surface, GRAY, (neck_x, neck_y, neck_width, neck_height))
+
+        torso_width = self.width * 0.7
+        torso_height = self.height * 0.3
+        torso_x = (self.width - torso_width) // 2
+        torso_y = neck_y + neck_height
+        pygame.draw.rect(surface, body_color, (torso_x, torso_y, torso_width, torso_height))
+
+        # Arms
+        arm_width = self.width * 0.15
+        arm_height = self.height * 0.3
+        arm_y = torso_y + 5
+        
+        # Left arm
+        pygame.draw.rect(surface, body_color, (torso_x - arm_width, arm_y, arm_width, arm_height))
+        # Right arm
+        pygame.draw.rect(surface, body_color, (torso_x + torso_width, arm_y, arm_width, arm_height))
+
+        # Hands (small circles)
+        hand_radius = 4
+        pygame.draw.circle(surface, GRAY, (torso_x - arm_width + arm_width // 2, arm_y + arm_height), hand_radius)
+        pygame.draw.circle(surface, GRAY, (torso_x + torso_width + arm_width // 2, arm_y + arm_height), hand_radius)
+
+        # Hips
+        pelvis_width = self.width * 0.8
+        pelvis_height = self.height * 0.1
+        pelvis_x = (self.width - pelvis_width) // 2
+        pelvis_y = torso_y + torso_height
+        pygame.draw.rect(surface, body_color, (pelvis_x, pelvis_y, pelvis_width, pelvis_height))
+
+        # Legs (two rectangles for thighs and shins)
+        leg_segment_width = pelvis_width // 2 - 4
+        leg_segment_height = self.height * 0.2
+        
+        # Left thigh
+        thigh_y = pelvis_y + pelvis_height
+        pygame.draw.rect(surface, body_color, (pelvis_x, thigh_y, leg_segment_width, leg_segment_height))
+        # Right thigh - FIX: Changed 'pelvel_x' to 'pelvis_x'
+        pygame.draw.rect(surface, body_color, (pelvis_x + pelvis_width - leg_segment_width, thigh_y, leg_segment_width, leg_segment_height))
+
+        # Left shin
+        shin_y = thigh_y + leg_segment_height
+        pygame.draw.rect(surface, body_color, (pelvis_x, shin_y, leg_segment_width, leg_segment_height))
+        # Right shin
+        pygame.draw.rect(surface, body_color, (pelvis_x + pelvis_width - leg_segment_width, shin_y, leg_segment_width, leg_segment_height))
+
+        # Feet 
+        foot_width = leg_segment_width + 2
+        foot_height = 5
+        foot_y = shin_y + leg_segment_height
+        pygame.draw.rect(surface, BLACK, (pelvis_x, foot_y, foot_width, foot_height))
+        pygame.draw.rect(surface, BLACK, (pelvis_x + pelvis_width - foot_width, foot_y, foot_width, foot_height))
+
+        # Face details
+        pygame.draw.line(surface, BLACK, (head_center_x - head_radius // 3, head_center_y - head_radius // 4), (head_center_x + head_radius // 3, head_center_y - head_radius // 4), 2) # Frowning eyes
+        pygame.draw.line(surface, BLACK, (head_center_x - head_radius // 3, head_center_y + head_radius // 4), (head_center_x + head_radius // 3, head_center_y + head_radius // 4), 1) # Simple mouth
+
+
+    def update(self, platforms):
+        self.rect.x += self.speed * self.direction
+        
+        # Check if enemy hits screen edges or platform edges
+        if self.rect.left < 0 or self.rect.right > SCREEN_WIDTH:
+            self.direction *= -1 # Change direction
+            self.rect.x += self.speed * self.direction * 2 # Move a bit to avoid sticking
+
+        self.rect.y += 2 
+        for platform in platforms:
+            if self.rect.colliderect(platform.rect):
+                if self.rect.bottom > platform.rect.top and self.rect.top < platform.rect.top:
+                    self.rect.bottom = platform.rect.top
+        
+        if self.rect.top > SCREEN_HEIGHT:
+            self.kill()
+
+
+    def take_damage(self, damage):
+        self.health -= damage
+        if self.health <= 0:
+            return True 
+        return False
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+        # Draw health bar for enemy
+        health_bar_width = self.rect.width
+        health_bar_height = 5
+        health_bar_x = self.rect.x
+        health_bar_y = self.rect.y - 10
+        pygame.draw.rect(screen, RED, (health_bar_x, health_bar_y, health_bar_width, health_bar_height))
+        current_health_width = (self.health / self.max_health) * health_bar_width
+        pygame.draw.rect(screen, GREEN, (health_bar_x, health_bar_y, current_health_width, health_bar_height))
+
 
 
 
