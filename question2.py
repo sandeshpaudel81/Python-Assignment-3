1836dfc9968687cd6a23ec2e013a658f9cb5efad
binod basnet <vinodbasnet87@gmail.com>
2025-05-25 20:13:59 +1000

    feat: Class for platform is added

--- question2.py
+++ question2.py
@@ -386,4 +386,14 @@ class Collectible(pygame.sprite.Sprite):
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
-    
+
+# Class for platform
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        self.image = pygame.Surface((width, height))
+        self.image.fill(BROWN)
+        self.rect = self.image.get_rect(topleft=(x, y))
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
